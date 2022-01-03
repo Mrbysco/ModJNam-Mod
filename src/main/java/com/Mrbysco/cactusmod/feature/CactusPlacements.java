9e0e73dc0b9aaa9b3a30688e105ebfa49007d944
Mrbysco <Mrbysco@users.noreply.github.com>
2022-01-03 01:20:53 +0100

    Fix the cactus plant rarity config option being unused [push]

--- src/main/java/com/Mrbysco/cactusmod/feature/CactusPlacements.java
+++ src/main/java/com/Mrbysco/cactusmod/feature/CactusPlacements.java
@@ -1,12 +1,14 @@
 package com.mrbysco.cactusmod.feature;
 
+import com.mrbysco.cactusmod.config.CactusConfig;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
 import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
 
 public class CactusPlacements {
-	public static final PlacedFeature CACTUS_PLANT = PlacementUtils.register("chorus_plant", CactusFeatureConfig.CACTUS_PLANT.placed(CountPlacement.of(UniformInt.of(0, 4)), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome()));
+	public static final PlacedFeature CACTUS_PLANT = PlacementUtils.register("chorus_plant", CactusFeatureConfig.CACTUS_PLANT.placed(RarityFilter.onAverageOnceEvery(CactusConfig.COMMON.cactusPlantRarity.get()), CountPlacement.of(UniformInt.of(0, 4)), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome()));
 }
